b0a42b322bb9aa3d97361f41f16b6ac3b3d5e4b4
LostWeb45 <kostik052005@gmail.com>
2025-08-31 13:38:46 +0300

    Алгоритмы

---
+++ 10_cash.js
@@ -0,0 +1,31 @@
+function cashFunction(fn) {
+  const cash = {};
+  return function (n) {
+    if (cash[n]) {
+      console.log("Взято из кеша", cash[n]);
+      return cash[n];
+    }
+    let result = fn(n);
+    cash[n] = result;
+    console.log("Посчитала функция", cash[n]);
+    return result;
+  };
+}
+
+function factorial(n) {
+  let result = 1;
+  while (n != 1) {
+    result += n;
+    n -= 1;
+  }
+  return result;
+}
+
+const cashFactorial = cashFunction(factorial);
+
+cashFactorial(4);
+cashFactorial(5);
+cashFactorial(6);
+cashFactorial(7);
+cashFactorial(7);
+cashFactorial(4);
